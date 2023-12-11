f53bb259cebc93abfdde0ddf7540d0fa0e808f74
andreeabrindea <andreeabrindea61@gmail.com>
2023-12-12 01:12:25 +0200

    extracted integer, fraction and exponent for each corresponding part of the number

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -1,11 +1,24 @@
+using System;
 using System.Linq;
+using Microsoft.CodeAnalysis;
 
 namespace Json
 {
     public static class JsonNumber
     {
-        public static bool IsJsonNumber(string input) =>
-            IsInteger(input) || IsFraction(input) || IsExponentialExpression(input);
+        public static bool IsJsonNumber(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            int exponentIndex = input.IndexOfAny("eE".ToCharArray());
+            int dotIndex = input.IndexOf('.');
+            return IsInteger(Integer(input, dotIndex, exponentIndex))
+                   && IsFraction(Fraction(input, dotIndex, exponentIndex))
+                   && IsExponentialExpression(Exponent(input, exponentIndex));
+        }
 
         private static bool IsInteger(string number)
         {
@@ -19,70 +32,65 @@ namespace Json
 
         private static bool IsFraction(string fraction)
         {
-            if (string.IsNullOrEmpty(fraction))
-            {
-                return false;
-            }
-
-            int indexOfDecimalPoint = fraction.IndexOf('.');
-
-            return indexOfDecimalPoint > 0
-                   && AreDigits(fraction[..indexOfDecimalPoint])
-                   && AreDigits(fraction[(indexOfDecimalPoint + 1) ..]);
+            return AreDigits(fraction);
         }
 
-        private static bool IsExponentialExpression(string input)
+        private static bool IsExponentialExpression(string input) => IsValidExponentialExpression(input)
+                                                                     && input.Count(c => c == 'e') <= 1
+                                                                     && input.Count(c => c == 'E') <= 1;
+
+        private static string Integer(string input, int dotIndex, int exponentIndex)
         {
-            if (string.IsNullOrEmpty(input))
+            if (dotIndex > -1 && exponentIndex < 0)
             {
-                return false;
+                return input[.. dotIndex];
             }
 
-            if ((!input.Contains('e') && !input.Contains('E'))
-                || input.Count(c => c == 'E') > 1
-                || input.Count(c => c == 'e') > 1)
+            if (dotIndex > -1 && exponentIndex > -1)
             {
-                return false;
+                return input[.. dotIndex];
             }
 
-            int indexOfExponent = input.IndexOfAny("eE".ToCharArray());
-            string expression = GetExpressionAfterExponent(input, indexOfExponent);
-            return IsValidExponentialExpression(expression);
+            if (dotIndex < 0 && exponentIndex > -1)
+            {
+                return input[.. exponentIndex];
+            }
+
+            return input;
+        }
+
+        private static string Fraction(string input, int dotIndex, int exponentIndex)
+        {
+            if (dotIndex > -1 && exponentIndex < 0)
+            {
+                return input[(dotIndex + 1) ..];
+            }
+
+            if (dotIndex > -1 && exponentIndex > -1)
+            {
+                return input[(dotIndex + 1) ..exponentIndex];
+            }
+
+            return "0";
         }
 
         private static bool IsValidExponentialExpression(string expression)
         {
             int signIndex;
-            char[] acceptedOperands = { '+', '-' };
 
             if (expression.StartsWith('-') || expression.StartsWith('+'))
             {
-                signIndex = expression.IndexOfAny(acceptedOperands);
+                signIndex = expression.IndexOfAny("+-".ToCharArray());
                 expression = expression[(signIndex + 1) ..];
             }
 
             return AreDigits(expression);
         }
 
-        private static string GetExpressionAfterExponent(string input, int indexOfExponent)
-        {
-            if (input.Contains('e') || input.Contains('E'))
-            {
-                return indexOfExponent != -1
-                    ? input[(indexOfExponent + 1) ..]
-                    : string.Empty;
-            }
-
-            return string.Empty;
-        }
+        private static string Exponent(string input, int indexOfExponent) => indexOfExponent != -1 ? input[(indexOfExponent + 1) ..] : "0";
 
         private static bool AreDigits(string number)
         {
-            if (string.IsNullOrEmpty(number))
-            {
-                return false;
-            }
-
             foreach (var digit in number)
             {
                 if (!char.IsDigit(digit))
@@ -96,8 +104,7 @@ namespace Json
 
         private static bool ItStartsWithZeroAndHasMoreThan1Digit(string input) => input.StartsWith('0') && input.Length > 1;
 
-        private static bool IsNegativeInteger(string input) => !string.IsNullOrEmpty(input)
-                                                               && input[0] == '-'
+        private static bool IsNegativeInteger(string input) => input[0] == '-'
                                                                && input.Length > 1;
     }
 }
