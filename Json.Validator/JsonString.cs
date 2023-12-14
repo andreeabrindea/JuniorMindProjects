4b48d6528388adb493fb61050665445934df05a0
andreeabrindea <andreeabrindea61@gmail.com>
2023-12-15 01:20:11 +0200

    case uncomplete unicode done

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -4,28 +4,46 @@ namespace Json
 {
     public static class JsonString
     {
-        public static bool IsJsonString(string input)
+       public static bool IsJsonString(string input)
         {
             if (string.IsNullOrEmpty(input))
             {
                 return false;
             }
 
-            return AreCharacters(input) && !IsEndingWithBackslash(input);
+            if (input.Length == 2)
+            {
+                return true;
+            }
+
+            return AreCharacters(input);
         }
 
-        private static bool IsDoubleQuoted(string input) => input.StartsWith('\"') && input.EndsWith('\"');
+       private static bool IsDoubleQuoted(string input) => input.StartsWith('\"') && input.EndsWith('\"');
 
-        private static bool AreCharacters(string input)
+       private static bool AreCharacters(string input)
         {
             if (!IsDoubleQuoted(input))
             {
                 return false;
             }
 
-            foreach (var c in input)
+            int endIndex = input.Length - 2;
+            input = input[.. (endIndex + 1)];
+
+            if (input.EndsWith('\\'))
             {
-                if (!IsCharacter(c) && !IsEscape(c) || char.IsControl(c))
+                return false;
+            }
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i - 1] == '\\')
+                {
+                    return IsEscape(input[i], input);
+                }
+
+                if (!IsCharacter(input[i]) && !IsEscape(input[i], input))
                 {
                     return false;
                 }
@@ -34,55 +52,55 @@ namespace Json
             return true;
         }
 
-        private static bool IsCharacter(char c)
+       private static bool IsCharacter(char c)
         {
             const int lowerBound = 32;
-            return Convert.ToInt32(c) >= lowerBound;
+            return Convert.ToInt32(c) >= lowerBound && c != '\"' && c != '\\';
         }
 
-        private static bool IsEscape(char c)
+       private static bool IsEscape(char c, string input)
         {
-            if (c.ToString().StartsWith("\\u"))
+            switch (c)
             {
-                return IsCompleteHexadecimalUnicode(c);
-            }
-
-            const string pattern = "\\\"\b\f\n\r\t\\/";
-            return pattern.Contains(c);
-        }
-
-        private static bool IsEndingWithBackslash(string input)
-        {
-            int indexOfLastElement = input.Length - 2;
-            return input[indexOfLastElement] == '\\';
-        }
-
-        private static bool IsCompleteHexadecimalUnicode(char c)
-        {
-            string escapeUnicodeString = c.ToString();
-            if (escapeUnicodeString.Length < 6)
-            {
-                return false;
-            }
-
-            if (!escapeUnicodeString.StartsWith("\\u"))
-            {
-                return false;
-            }
-
-            const int startIndexForHex = 2;
-            foreach (var character in escapeUnicodeString[startIndexForHex..])
-            {
-                if (!IsHex(character))
-                {
+                case '\"':
+                case '\\':
+                case '/':
+                case 'b':
+                case 'f':
+                case 'r':
+                case 't':
+                case 'n':
+                    return true;
+                case 'u':
+                    return IsCompleteHexadecimalUnicode(input);
+                default:
                     return false;
-                }
             }
-
-            return true;
         }
 
-        private static bool IsHex(char c)
+       private static bool IsCompleteHexadecimalUnicode(string input)
+       {
+           int indexOfCharacterU = input.IndexOf("u");
+           const int unicodeLength = 5;
+           int endIndex = indexOfCharacterU + unicodeLength;
+           if (input.Length < endIndex)
+           {
+               return false;
+           }
+
+           input = input[(indexOfCharacterU + 1) ..endIndex];
+           foreach (var c in input)
+           {
+               if (!IsHex(c))
+               {
+                   return false;
+               }
+           }
+
+           return true;
+       }
+
+       private static bool IsHex(char c)
         {
             c = char.ToLower(c);
             return c >= '0' && c <= '9' || c >= 'a' && c <= 'f';
