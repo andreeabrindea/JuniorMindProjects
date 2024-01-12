504998bd3f6c09e24101f6736de7800d3a2851df
andreeabrindea <andreeabrindea61@gmail.com>
2024-01-12 14:11:12 +0200

    removed player facts and added small test for team

--- FootballRanking/FootballRanking/TeamFacts.cs
+++ FootballRanking/FootballRanking/TeamFacts.cs
@@ -7,5 +7,8 @@ public class TeamFacts
     [Fact]
     public void AddPointsTest()
     {
+        Team testTeam = new Team("test team");
+        testTeam.AddPoints(3);
+        Assert.Equal(3, testTeam.GetPoints());
     }
 }
