791769a8312982d25859e000add3eb7cf0019d3c
andreeabrindea <andreeabrindea61@gmail.com>
2024-02-22 13:30:19 +0200

    implemented Value & added Console app.

---
+++ JsonValidator/Json.Validator/Value.cs
@@ -0,0 +1,51 @@
+namespace Json;
+
+public class Value : IPattern
+{
+    private readonly IPattern pattern;
+
+    public Value()
+    {
+        var ws = new Many(new Any(" \n\r\t"));
+        var value = new Choice(
+            new String(),
+            new Number(),
+            new Text("true"),
+            new Text("false"),
+            new Text("null"));
+
+        var element = new Sequence(ws, value, ws);
+
+        var array = new Sequence(
+            new Character('['),
+            ws,
+            new List(element, new Sequence(new Character(','))),
+            ws,
+            new Character(']'));
+        value.Add(array);
+
+        var member = new Sequence(
+            ws,
+            new String(),
+            ws,
+            new Character(':'),
+            element);
+
+        var objectPattern = new Sequence(
+            new Character('{'),
+            ws,
+            new List(
+                member,
+                new Character(',')),
+            ws,
+            new Character('}'));
+        value.Add(objectPattern);
+
+        pattern = element;
+    }
+
+    public IMatch Match(string text)
+    {
+        return pattern.Match(text);
+    }
+}
---
+++ JsonValidator/JsonConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Json.JsonConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string lines;
+            try
+            {
+                lines = File.ReadAllText(args[0]);
+                Console.WriteLine(lines);
+                Value jsonValidator = new Value();
+                
+                Console.WriteLine(jsonValidator.Match(lines).Success()
+                    ? "The file respects the JSON format."
+                    : "The file does not respect the JSON format.");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
