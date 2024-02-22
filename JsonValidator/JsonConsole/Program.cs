aa77f973d1890c3af7289c824c59a456ff2c0221
andreeabrindea <andreeabrindea61@gmail.com>
2024-02-22 15:43:33 +0200

    refactored Main function to give user more information, removed exeption and verified the remaining of the text too

--- JsonValidator/JsonConsole/Program.cs
+++ JsonValidator/JsonConsole/Program.cs
@@ -4,19 +4,29 @@
     {
         public static void Main(string[] args)
         {
-            string content;
-            try
+            if (args.Length < 1)
             {
-                content = File.ReadAllText(args[0]);
-
-                Value jsonValidator = new Value();
-                Console.WriteLine(jsonValidator.Match(content).Success()
-                    ? "The file respects the JSON format."
-                    : "The file does not respect the JSON format.");
+                Console.WriteLine("No file provided.");
+                return;
             }
-            catch (FileNotFoundException e)
+            
+            if (!File.Exists(args[0]))
             {
-                Console.WriteLine("Exception: " + e.Message);
+                Console.WriteLine("No file with the given path was found. Ensure that you enter the path to the file correctly.");
+                return;
+            }
+
+            string content;
+            content = File.ReadAllText(args[0]);
+            Value jsonValidator = new Value();
+            
+            if (jsonValidator.Match(content).Success() && jsonValidator.Match(content).RemainingText() == string.Empty)
+            {
+                Console.WriteLine("The file respects the JSON format.");
+            }
+            else
+            {
+                Console.WriteLine("The file does not respect the JSON format. Ensure that the content of the file is correct \n " + jsonValidator.Match(content).RemainingText());
             }
         }
     }
