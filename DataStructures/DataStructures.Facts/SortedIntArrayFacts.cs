5eb4601f5ebc8e6ec0c98d8e7f1ce0a6581ace1e
andreeabrindea <andreeabrindea61@gmail.com>
2024-05-14 00:03:18 +0300

    tested if removeAt works on sorted array

--- DataStructures/DataStructures.Facts/SortedIntArrayFacts.cs
+++ DataStructures/DataStructures.Facts/SortedIntArrayFacts.cs
@@ -5,7 +5,7 @@ namespace DataStructures.Facts;
 public class SortedIntArrayFacts
 {
     [Fact]
-    public void Test_AddASingleElement()
+    public void AddASingleElement()
     {
         SortedIntArray sortedIntArray = new();
         sortedIntArray.Add(5);
@@ -15,7 +15,7 @@ public class SortedIntArrayFacts
     }
 
     [Fact]
-    public void Test_AddMultipleElements()
+    public void AddMultipleElements()
     {
         SortedIntArray sortedIntArray = new SortedIntArray();
         sortedIntArray.Add(59);
@@ -32,6 +32,22 @@ public class SortedIntArrayFacts
         Assert.Equal(43, sortedIntArray[4]);
         Assert.Equal(59, sortedIntArray[5]);
         Assert.Equal(0, sortedIntArray.IndexOf(6));
+    }
+    
+    [Fact]
+    public void RemoveFromExistingPosition()
+    {
+        SortedIntArray sortedIntArray = new();
+        sortedIntArray.Add(80);
+        sortedIntArray.Add(10);
+        sortedIntArray.Add(20);
+        
+        sortedIntArray.RemoveAt(1);
+        
+        Assert.Equal(10, sortedIntArray[0]);
+        Assert.Equal(80, sortedIntArray[1]);
+        Assert.Equal(-1, sortedIntArray.IndexOf(20));
+        Assert.Equal(2, sortedIntArray.Count);
     }
     
 }
