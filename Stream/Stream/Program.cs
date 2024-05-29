d6d2eb0e113b1fdae7780cd443a2feb1df943fec
andreeabrindea <andreeabrindea61@gmail.com>
2024-05-30 00:05:53 +0300

    created functions for reading & writing to a stream

---
+++ Stream/Stream/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+
+    public static void Main()
+    {
+        
+    }
+    public static string ReadStream(Stream stream)
+    {
+        return new StreamReader(stream).ReadToEnd();
+    }
+
+    public static void WriteStream(Stream stream, bool gzip = false, bool crypt = false)
+    {
+        string content = ReadStream(stream);
+        StreamWriter writer = new StreamWriter(content);
+        writer.Write(content);
+        writer.Flush();
+    }
+    
+}
