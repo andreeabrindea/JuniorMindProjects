95e131c0e1ef20f2c001a301b60a4002c85e1a32
andreeabrindea <andreeabrindea61@gmail.com>
2024-05-30 11:49:54 +0300

    verified if functions work

--- Stream/Stream/Program.cs
+++ Stream/Stream/Program.cs
@@ -3,19 +3,26 @@
 
     public static void Main()
     {
+        FileStream inputFile = new ("/Users/andreea/Projects/JSON_Validator/Stream/Stream/file.txt", 
+            FileMode.Open, FileAccess.Read, FileShare.Read);
         
+        FileStream outputFile = new("/Users/andreea/Projects/JSON_Validator/Stream/Stream/output.txt", 
+            FileMode.Open, FileAccess.Write, FileShare.None);
+        
+        Console.WriteLine(ReadStream(inputFile));
+        inputFile.Position = 0;
+        WriteStream(inputFile, outputFile);
     }
     public static string ReadStream(Stream stream)
     {
         return new StreamReader(stream).ReadToEnd();
     }
 
-    public static void WriteStream(Stream stream, bool gzip = false, bool crypt = false)
+    public static void WriteStream(Stream stream, Stream output, bool gzip = false, bool crypt = false)
     {
         string content = ReadStream(stream);
-        StreamWriter writer = new StreamWriter(content);
+        StreamWriter writer = new StreamWriter(output);
         writer.Write(content);
         writer.Flush();
     }
-    
 }
