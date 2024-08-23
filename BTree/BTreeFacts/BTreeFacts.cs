63b58f1979b2b6aea4c5555be7862f00df257934
andreeabrindea <andreeabrindea61@gmail.com>
2024-08-23 13:39:45 +0300

    test: Added tests for Add and Search methods

--- BTree/BTreeFacts/BTreeFacts.cs
+++ BTree/BTreeFacts/BTreeFacts.cs
@@ -3,10 +3,29 @@ using Xunit;
 public class BTreeFacts
 {
     [Fact]
-    public void Test1()
+    public void AddAKeyAndSearchForIt()
     {
-        BTreeCollection<int> treeCollection = new();
-        treeCollection.Add(2);
-        
+        BTreeCollection<int> btree = new();
+        btree.Add(2);
+        Assert.True(btree.Search(2));
+        Assert.False(btree.Search(3));
+    }
+
+    [Fact]
+    public void AddSeveralKeys()
+    {
+        BTreeCollection<int> btree = new(4);
+        btree.Add(59);
+        btree.Add(7);
+        btree.Add(23);
+        btree.Add(73);
+        btree.Add(97);
+        Assert.True(btree.Search(7));
+        Assert.True(btree.Search(59));
+        Assert.True(btree.Search(23));
+        Assert.True(btree.Search(73));
+        Assert.True(btree.Search(97));
+        Assert.False(btree.Search(66));
+
     }
 }
