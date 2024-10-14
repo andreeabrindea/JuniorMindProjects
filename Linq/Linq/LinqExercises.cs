dc888053680dd0476cb445d6e8ff036b3d9dd0d4
andreeabrindea <andreeabrindea61@gmail.com>
2024-10-14 12:15:14 +0300

    feat: Implement GetFirstCharacterThatDoesNotRepeat

--- Linq/Linq/LinqExercises.cs
+++ Linq/Linq/LinqExercises.cs
@@ -5,4 +5,7 @@ public class LinqExercises
     public static (int, int) GetNoOfConsonantsAndVowels(string s) =>
         (s.Count(character => !"aeiou".Contains(character) && char.IsLetter(character)),
             s.Count(character => "aeiou".Contains(character) && char.IsLetter(character)));
+
+    public static char GetFirstCharacterThatDoesNotRepeat(string s) =>
+        s.ToLookup(p => p).First(p => p.Count() == 1).Key;
 }
