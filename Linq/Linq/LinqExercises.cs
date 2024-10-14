01945441f166c8e65f370ee25dc2e0cfbfcc0ec3
andreeabrindea <andreeabrindea61@gmail.com>
2024-10-14 12:19:54 +0300

    feat: Implement ConvertsStringToInt

--- Linq/Linq/LinqExercises.cs
+++ Linq/Linq/LinqExercises.cs
@@ -8,4 +8,35 @@ public class LinqExercises
 
     public static char GetFirstCharacterThatDoesNotRepeat(string s) =>
         s.ToLookup(p => p).First(p => p.Count() == 1).Key;
+
+    public static int ConvertsStringToInt(string s)
+    {
+        const int ten = 10;
+        var result = s.Aggregate(0, (accumulate, character) =>
+        {
+            if (character < '0' || character > '9')
+            {
+                throw new FormatException(nameof(s));
+            }
+
+            return accumulate * ten + (character - '0');
+        });
+        return GetSign(s) * result;
+    }
+
+    private static int GetSign(string s)
+    {
+        int sign = 1;
+        if (s[0] == '-')
+        {
+            sign = -1;
+        }
+
+        if (char.IsDigit(s[0]) || s[0] != '+')
+        {
+            throw new FormatException("The string cannot be a valid number");
+        }
+
+        return sign;
+    }
 }
