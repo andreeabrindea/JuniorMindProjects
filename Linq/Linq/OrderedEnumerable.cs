65972a07e6ca887667a41571fe407e2464a8c47f
andreeabrindea <andreeabrindea61@gmail.com>
2024-10-15 15:17:49 +0300

    fix: Make Sorting in GetEnumerator() with list of comparers

--- Linq/Linq/OrderedEnumerable.cs
+++ Linq/Linq/OrderedEnumerable.cs
@@ -5,31 +5,46 @@ namespace Linq;
 public class OrderedEnumerable<TSource, TKey> : IOrderedEnumerable<TSource>
 {
     private readonly List<TSource> source;
-    private readonly bool descending;
 
     public OrderedEnumerable(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IComparer<TKey> comparer, bool descending)
     {
         this.source = source.ToList();
-        this.descending = descending;
         if (descending)
         {
-            this.source.Sort((x, y) => comparer.Compare(keySelector(y), keySelector(x)));
+            SortComparers.Add((x, y) => comparer.Compare(keySelector(y), keySelector(x)));
         }
         else
         {
-            this.source.Sort((x, y) => comparer.Compare(keySelector(x), keySelector(y)));
+            SortComparers.Add((x, y) => comparer.Compare(keySelector(x), keySelector(y)));
         }
     }
+
+    public List<Func<TSource, TSource, int>> SortComparers { get; } = new();
 
     public IOrderedEnumerable<TSource> CreateOrderedEnumerable<TKey>(
         Func<TSource, TKey> keySelector, IComparer<TKey>? comparer, bool descending)
     {
-        return new OrderedEnumerable<TSource, TKey>(
-            source.OrderBy(keySelector, comparer).ToList(), keySelector, comparer, descending);
+        var newOrderedEnumerable = new OrderedEnumerable<TSource, TKey>(source, keySelector, comparer, descending);
+        newOrderedEnumerable.SortComparers.AddRange(SortComparers);
+        return newOrderedEnumerable;
     }
 
     public IEnumerator<TSource> GetEnumerator()
     {
+        source.Sort((x, y) =>
+        {
+            foreach (var comparer in SortComparers)
+            {
+                var result = comparer(x, y);
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+
+            return 0;
+        });
+
         foreach (var s in source)
         {
             yield return s;
