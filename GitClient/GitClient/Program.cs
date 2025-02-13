eb8d4039a8edc935c3864dabf5125416df070400
andreeabrindea <andreeabrindea61@gmail.com>
2025-02-13 14:11:58 +0200

    feat: display information on different columns with specific color each

--- GitClient/GitClient/Program.cs
+++ GitClient/GitClient/Program.cs
@@ -5,9 +5,18 @@ internal class Program
     public static void Main(string[] args)
     {
         GitClient gitClient = new(Directory.GetCurrentDirectory());
+        const int codeForYellow = 33;
+        const int codeForBlue = 34;
+        const int codeForMagenta = 35;
+        const int codeForGreen = 36;
         foreach (var r in gitClient.GetCommits())
         {
-            Console.WriteLine(r.Author + " " + r.Date + " " + r.Message);
+            Console.WriteLine(
+                " {0, 10}  {1, 10}  {2, 10}  {3, 10} ",
+                $"\x1b[{codeForYellow}m{r.Hash}\x1b[0m",
+                $"\x1b[{codeForBlue}m{r.Date}\x1b[0m",
+                $"\x1b[{codeForMagenta}m{r.Author}\x1b[0m",
+                $"\x1b[{codeForGreen}m{r.Message}");
         }
     }
 }
